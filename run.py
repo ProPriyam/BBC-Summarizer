a0db434c7c36ad641761d295f72a982c6615a969
Priyam Gutpa <44114259+ProPriyam@users.noreply.github.com>
2023-08-06 11:34:58 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ openai_api_key = ""  # Replace with your API key
 
 summarizer = NewsSummarizer(url="https://bbc.com/news")
 summarizer.get_articles(
-    openai_api_key=openai_api_key, hours=5, body=False, summarized=True
+    openai_api_key=openai_api_key, hours=5, body=False, summarized=False
 )
